b4ed1bb3581c46b9f0c3823478db93aae71185df
Leonardo Teodoro <38961152+imponateado@users.noreply.github.com>
2022-06-12 21:23:56 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Purtruet</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    </head>
+    <body>
+        <form action="sortpage.php" method="POST">
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <br>
+                        <div class="form-group">
+                            <textarea class="form-control" name="wppbkp" rows="10" placeholder="put wpp bkp content here"></textarea>
+                        </div>
+                    </div>
+                </div>
+                <br>
+                <div class="row">
+                    <div class="col">
+                        <input class="form-control" type="text" placeholder="Delimiter" name="delimiter">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <br>
+                        <input type="submit" class="btn btn-success" value="Done">
+                    </div>
+                </div>
+            </div>
+        </form>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+    </body>
+</html>
---
+++ sortpage.php
@@ -0,0 +1,16 @@
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Purtruet</title>
+        <link rel="stylesheet" href="css/bootstrap.css">
+    </head>
+    <body>
+        <?php
+            echo $_POST['wppbkp'];
+            echo $_POST['delimiter'];
+        ?>
+        <script src="js/bootstrap.js"></script>
+    </body>
+</html>
